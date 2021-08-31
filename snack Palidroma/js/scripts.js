83cb85afa854e9892a2a853b21b0551a53ba3da0
IlarioCOCCO <ilario.cocco@gmail.com>
2021-08-31 15:33:25 +0200

    commi exercise 1 palindrome

--- snack Palidroma/js/scripts.js
+++ snack Palidroma/js/scripts.js
@@ -5,14 +5,15 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 function Ispalindrome(parola) {
+    for (var i = 0; i <= parola.length; i--);
     if (parola){
         return true;
     }
-        return true;
+        return false;
 }
 
 var parola = prompt("inserisci una Parola")
     console.log(parola);
     alert(parola);
 
-for(var i=0; i<= parola.length; i--);
+// for(var i=0; i<= parola.length; i--);
