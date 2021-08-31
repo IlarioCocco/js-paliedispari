8406aa5d3237cf418353e3543566a54a9aca6971
IlarioCOCCO <ilario.cocco@gmail.com>
2021-08-31 15:28:17 +0200

    commit palindrome

--- snack Palidroma/js/scripts.js
+++ snack Palidroma/js/scripts.js
@@ -4,3 +4,15 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
+function Ispalindrome(parola) {
+    if (parola){
+        return true;
+    }
+        return true;
+}
+
+var parola = prompt("inserisci una Parola")
+    console.log(parola);
+    alert(parola);
+
+for(var i=0; i<= parola.length; i--);
